a3df7a97ada1301add659af6764ec30bfa618790
Ian Johnson <tacoda@hey.com>
2025-06-18 14:27:11 +0100

    Add password resources

--- README.md
+++ README.md
@@ -19,9 +19,53 @@ r g devise User
 r db:migrate
 ```
 
-## Adding Models
+## Adding Initial Models
 
 ```sh
 r g model Password url username password
 r g model UserPassword user:belongs_to password:belongs_to
 ```
+
+- Add model relations
+
+## Adding ActiveRecord Encryption
+
+```sh
+r db:encryption:init
+r credentials:edit --environment=development
+# Generates master key for environment
+```
+
+- Add model encrypt calls
+
+```sh
+r db:migrate
+r c
+```
+
+```ruby
+Password.create!(url: "twitter.com", username: "whatever", password: "password1234$")
+Password.last.username
+Password.last.password
+Password.find_by_username("whatever")
+Password.where(username: "whatever")
+```
+
+## Add UI
+
+- Add password routes
+- Add password controller
+
+```sh
+bin/dev
+```
+
+- Update application layout
+- Add password index view
+- Add new and create actions
+- Add new view and form partial
+- Add password model validations
+- Add error rendering to form partial
+- Add password show action
+- Extract lookup to before action
+- Add show view
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_17_144409) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_145300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "passwords", force: :cascade do |t|
+    t.string "url"
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_passwords", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "password_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["password_id"], name: "index_user_passwords_on_password_id"
+    t.index ["user_id"], name: "index_user_passwords_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_17_144409) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "user_passwords", "passwords"
+  add_foreign_key "user_passwords", "users"
 end
